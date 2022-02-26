f26a21229a84a602dca680b477068c14d6ae11ff
nicolasmilitello <nicolas.militello@hotmail.com>
2022-02-26 20:55:15 -0300

    normalize file added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Timer from './Timer';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Timer from "./Timer";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Timer />, document.getElementById('root')
-);
+import "./normalize.css";
+
+ReactDOM.render(<Timer />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
